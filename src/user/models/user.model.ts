faba9bfd9cda6a18d7cf34df3de2ad059f545d95
daseul22 <tlaektmf27@gmail.com>
2022-06-23 17:57:01 +0900

    init: 보일러 플레이팅 2/3"

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType, Int, registerEnumType } from '@nestjs/graphql';
+import { Field, ObjectType, registerEnumType, ID } from '@nestjs/graphql';
 
 export enum IdentityProvider {
   EMAIL,
@@ -6,13 +6,49 @@ export enum IdentityProvider {
   KAKAO,
   APPLE,
 }
-
 registerEnumType(IdentityProvider, {
   name: 'IdentityProvider',
 });
 
-@ObjectType()
+export enum UserGender {
+  MALE,
+  FEMALE,
+}
+
+registerEnumType(UserGender, {
+  name: 'UserGender',
+});
+
+export enum UserPermissionRole {
+  USER,
+  ADMIN,
+}
+
+registerEnumType(UserPermissionRole, {
+  name: 'UserPermissionRole',
+});
+
+@ObjectType({
+  description:
+    ' DateTime은 https://docs.nestjs.com/graphql/scalars#override-a-default-scalar 참고',
+})
 export class User {
-  @Field(() => Int)
+  @Field(() => ID)
   id: number;
+  username?: string;
+  name: string;
+  @Field(() => UserGender)
+  gender: UserGender;
+  phone: string;
+  email: string;
+  emailVerified?: string;
+  password?: string;
+  avatar?: string;
+  createdDate: Date;
+  updatedDate?: Date;
+  @Field(() => IdentityProvider)
+  identityProvider: IdentityProvider;
+  identityProviderId?: string;
+  @Field(() => UserPermissionRole)
+  role: UserPermissionRole;
 }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,35 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import {
+  IdentityProvider,
+  User,
+  UserGender,
+  UserPermissionRole,
+} from './models/user.model';
+import { UserService } from './user.service';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Query(() => User)
+  async user() {
+    console.log(this.userService);
+
+    return {
+      id: 1,
+      username: '',
+      name: '',
+      gender: UserGender.MALE,
+      phone: '',
+      email: '',
+      emailVerified: '',
+      password: '',
+      avatar: '',
+      createdDate: new Date('2022-06-23'),
+      updatedDate: new Date(),
+      identityProvider: IdentityProvider.GOOGLE,
+      identityProviderId: '',
+      role: UserPermissionRole.ADMIN,
+    };
+  }
+}
